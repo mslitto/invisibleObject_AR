5b03ddef221bd65e5b58d8915871eed8d5a09a24
litto.eu <litto.eu@gmail.com>
2020-03-29 14:13:40 +0200

    update scrape.js

--- js/scrape.js
+++ js/scrape.js
@@ -1,4 +1,3 @@
-
 const split = b => {
   const matches = b.match(/(\s*"[^"]+"\s*|\s*[^,]+|,)(?=,|$)/g);
   if (!matches) {
@@ -20,24 +19,39 @@ const load = async () => {
   }
 
   try {
-    const response = await fetch('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-21-2020.csv')
+    const date = new Date()
+    let month = date.getMonth() + 1
+    let day = date.getDate() - 1
+    if (day <= 9) {
+      day = `0${day}`
+    }
+    if (month <= 9) {
+      month = `0${month}`
+    }
+    
+    const url = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/${month}-${day}-2020.csv`
+    console.log({url})
+    const response = await fetch(url)
 
-	const body = await response.text()
+  const body = await response.text()
 
     body.trim().split('\n').map((line, i) => {
       if (i === 0) {
         return
       }
-
       const [
+        fips,
+        admin2,
         province,
         country,
         timestamp,
+        lat,
+        long,
         confirmed,
         deaths,
         recovered,
-        latitude,
-        longitude
+        active,
+        combined,
       ] = split(line)
 
       totals.confirmed += parseInt(confirmed, 10)
@@ -51,22 +65,21 @@ const load = async () => {
   }
   const defaultValue = -1;
   const deathRatio = (totals.confirmed / totals.deaths);
-  const deathBattle = ((totals.deaths / totals.confirmed) - (totals.recovered/ totals.confirmed));
+  const deathBattle = ((totals.recovered / totals.confirmed) - (totals.deaths/ totals.confirmed));
   const sickatHome = (totals.confirmed - totals.deaths - totals.recovered);
   
   //scale sphere add EventListener
   if (deathBattle < defaultValue) {
       console.log(1);
-  }else {
+  } else {
       console.log(0);
   }
   
 
   
-  document.id=figure.innerHTML = "confirmed " + totals.confirmed + "<br>" + "death " + totals.deaths + "<br>" + "recovered "  + totals.recovered + "<br>" + "sickatHome " + sickatHome ;
+  figure.innerHTML = "confirmed " + totals.confirmed + "<br>" + "death " + totals.deaths + "<br>" + "recovered "  + totals.recovered + "<br>" + "sickatHome " + sickatHome +  "<br>" + deathBattle;
 }
 
 
 
 load()
-
